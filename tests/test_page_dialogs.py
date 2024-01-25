1b4581ea7e95fa65820e6335954a40c28f19bca2
Ilya S <ISmeneger@yandex.ru>
2024-01-25 22:23:11 +0300

    Commit 25.01

---
+++ tests/test_page_dialogs.py
@@ -0,0 +1,32 @@
+import time
+from pages.modal_dialogs import ModalDialogs
+from pages.demoqa import DemoQa
+
+def test_modal_elements(browser):
+    modal_dialogs = ModalDialogs(browser)
+
+    modal_dialogs.visit()
+
+    assert modal_dialogs.btns_third_menu.check_count_elements(5)
+
+def test_navigation_modal(browser):
+    modal_dialogs = ModalDialogs(browser)
+    demo = DemoQa(browser)
+
+    demo.visit()
+    modal_dialogs.visit()
+
+    modal_dialogs.refresh()
+    modal_dialogs.icon.click()
+    browser.back()
+    browser.set_window_size(900, 400)
+    browser.forward()
+
+    assert demo.equal_url()
+    assert browser.title == 'DEMOQA'
+    browser.set_window_size(1000, 1000)
+
+
+
+
+
---
+++ tests/test_seo.py
@@ -0,0 +1,8 @@
+from pages.demoqa import DemoQa
+
+
+def test_seo(browser):
+    demo = DemoQa(browser)
+
+    demo.visit()
+    assert browser.title == 'DEMOQA'
