1069a748b48041b1580639e6cf2f2e181f84dccc
Ilya S <ISmeneger@yandex.ru>
2024-01-28 21:47:13 +0300

    Commit 28.01

--- tests/tests_hw/test_text_box.py
+++ tests/tests_hw/test_text_box.py
@@ -6,9 +6,22 @@ def test_text_box(browser):
     text_box = TextBox(browser)
 
     text_box.visit()
-    text_box.name.send_keys('Ilya S')
-    text_box.current_address.send_keys('Russia, Saint Petersburg')
+    name = 'Ilya S'
+    current_address = 'Russia, Saint Petersburg'
+
+    text_box.name.send_keys(name)
+    time.sleep(2)
+    text_box.current_address.send_keys(current_address)
+    time.sleep(2)
     text_box.btn_submit.click_force()
     assert text_box.text_appear.exist()
+    assert text_box.text_appear.get_text() == (f'Name:{name}\n'
+                                            f'Current Address :{current_address}')
+
+
+
+
+
+
 
 
